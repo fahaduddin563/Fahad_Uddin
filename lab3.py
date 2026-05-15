a739d6fd23a6016f9ebc3f799407d07be1accb0b
Fahad Uddin <fahad.uddin563@myci.csuci.edu>
2026-05-14 22:28:22 -0700

    Switch fibonacci to recursive

--- lab3.py
+++ lab3.py
@@ -1,11 +1,15 @@
-def fibonacci_iterative(n, filename):
-    a, b = 0, 1
+def fibonacci_recursive(n):
+    if n <= 1:
+        return n
 
+    return fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2)
+
+
+def write_fibonacci_recursive(count, filename):
     with open(filename, "w") as f:
-        for _ in range(n):
-            f.write(str(a) + "\n")
-            a, b = b, a + b
+        for i in range(count):
+            f.write(str(fibonacci_recursive(i)) + "\n")
 
 
-fibonacci_iterative(25, "output/fibonacci_iterative.txt")
+write_fibonacci_recursive(25, "output/fibonacci_recursive.txt")
 
